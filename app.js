0a996aa3422bb60bbe3538306b3945ae28d50022
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-06-05 17:12:49 +0200

    feat(express) add base routes

--- app.js
+++ app.js
@@ -14,14 +14,27 @@ function main() {
 
     const indexRoutes = require('./routing/index')
     app.use('/', indexRoutes)
+
     const getSongRoutes = require('./routing/get/song')
     app.use('/api/get/song/', getSongRoutes)
+    const postSongRoutes = require('./routing/post/song')
+    app.use('/api/post/song/', getSongRoutes)
+
+    const getArtistRoutes = require('./routing/get/artist')
+    app.use('/api/get/artist/', getArtistRoutes)
+    const postArtistRoutes = require('./routing/post/artist')
+    app.use('/api/post/artist/', postArtistRoutes)
+
+    const getAlbumRoutes = require('./routing/get/album')
+    app.use('/api/get/album/', getAlbumRoutes)
+    const postAlbumRoutes = require('./routing/post/album')
+    app.use('/api/post/album/', postAlbumRoutes)
 
     const port = 3000
 
     const essentiaTest = require('./src/essentia')
     //essentiaTest(essentia)
-    essentiaTest.essentiaTestTwo()
+    //essentiaTest.essentiaTestTwo()
 
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
