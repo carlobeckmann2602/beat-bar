566a5b4874f7a92171cece93a44605795594421f
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-06-05 17:13:32 +0200

    refactor(postgres) move db config object to seperate file

---
+++ model/config.js
@@ -0,0 +1,9 @@
+const config = {
+    user: process.env.BEAT_BAR_DB_USER,
+    password: process.env.BEAT_BAR_DB_PASSWORD,
+    host: process.env.BEAT_BAR_DB_HOST,
+    database: process.env.BEAT_BAR_DB_DATABASE,
+    port: process.env.BEAT_BAR_DB_PORT
+}
+
+module.exports=config;
