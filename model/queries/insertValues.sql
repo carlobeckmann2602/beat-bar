b7d8f6aedbe8442722678760132d0ddc21bd335c
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-06-05 08:54:10 +0200

    feat(insertValues) add script to add sample data

---
+++ model/queries/insertValues.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.album(
+	name,
+	year)
+VALUES (
+	'Adventure Island', 2021);
+
+INSERT INTO public.artist(
+	name)
+VALUES (
+	'Purple Cat');
+
+INSERT INTO public.song(
+	title,
+	artist_id,
+	album_id,
+	year,
+	duration)
+VALUES (
+	'Storm Clods',
+	1,
+	1,
+	2021,
+	156000);
