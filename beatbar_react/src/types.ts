c3be8a6afd2344657b81da8a1679fdaeffd65d6e
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-09-04 10:29:40 +0200

    feat(cdn): add new songs

--- beatbar_react/src/types.ts
+++ beatbar_react/src/types.ts
@@ -10,35 +10,89 @@ export type AllSongsUrls = {
 };
 export const ALL_SONGS = [
   {
+    title: "Bathroom - Chill Background Music",
+    artist: "chillmore",
+    url: "http://localhost:3001/bathroom-chill-background-music_chillmore.mp3"
+  },
+  {
+    title: "Coffee Chill Out",
+    artist: "Roman Belov",
+    url: "http://localhost:3001/coffee-chill-out_romanbelov.mp3"
+  },
+  {
+    title: "Dont Stop Me (Abstract Furure Bass)",
+    artist: "AlexiAction",
+    url: "http://localhost:3001/dont-stop-me-abstract-future-bass_alexi-action.mp3"
+  },
+  {
     title: "Embrace",
     artist: "ItsWatR",
-    url: "http://localhost:3001/embrace_itswatr.mp3",
-    length: "2:22",
+    url: "http://localhost:3001/embrace_itswatr.mp3"
+  },
+  {
+    title: "Empty Mind",
+    artist: "Lofi_hour",
+    url: "http://localhost:3001/empty-mind_lofi-hour.mp3"
+  },
+  {
+    title: "Good Night",
+    artist: "FASSounds",
+    url: "http://localhost:3001/good-night_fassounds.mp3"
+  },
+  {
+    title: "Lofi Chill Medium Version",
+    artist: "Bo Dleasons",
+    url: "http://localhost:3001/lofi-chill-medium-version_bo-dleasons.mp3"
+  },
+  {
+    title: "lofi-study_fassounds.mp3",
+    artist: "FASSounds",
+    url: "http://localhost:3001/lofi-study_fassounds.mp3"
+  },
+  {
+    title: "Lost Ambient Lofi 60s",
+    artist: "Lesfm",
+    url: "http://localhost:3001/lost-ambient-lofi-60s_lesfm.mp3"
+  },
+  {
+    title: "Rain and Nostalgia (60s Lofi)",
+    artist: "Lesfm",
+    url: "http://localhost:3001/rain-and-nostalgia-version-60s_lesfm.mp3"
   },
   {
     title: "Spirit Blossom",
-    artist: "RomanBelov",
-    url: "http://localhost:3001/spirit-blossom_roman-belov.mp3",
-  },
-  {
-    title: "LoFi Chill (Medium Version)",
-    artist: "BoDleasons",
-    url: "http://localhost:3001/lofi-chill-medium-version_bo-dleasons.mp3",
-  },
-  {
-    title: "Lofi Study",
-    artist: "FASSounds",
-    url: "http://localhost:3001/lofi-study_fassounds.mp3",
+    artist: "Roman Belov",
+    url: "http://localhost:3001/spirit-blossom_roman-belov.mp3"
   },
   {
     title: "Storm Clouds",
     artist: "Purple Cat",
-    url: "http://localhost:3001/storm-clouds_purpple-cat.mp3",
+    url: "http://localhost:3001/storm-clouds_purpple-cat.mp3"
   },
   {
-    title: "Watr-Fluid",
+    title: "Street Food",
+    artist: "FASSounds",
+    url: "http://localhost:3001/street-food_fassounds.mp3"
+  },
+  {
+    title: "Sunset Vibes (Lo-Fi/Chillhop)",
+    artist: "23843807",
+    url: "http://localhost:3001/sunset-vibes-lo-fichillhop_coma-media.mp3"
+  },
+  {
+    title: "The Weekend",
+    artist: "Chilmore",
+    url: "http://localhost:3001/the-weekend_chillmore.mp3"
+  },
+  {
+    title: "Tokyo Cafe",
+    artist: "TVARI",
+    url: "http://localhost:3001/tvari-tokyo-cafe_tvari.mp3"
+  },
+  {
+    title: "Watr Fluid",
     artist: "ItsWatR",
-    url: "http://localhost:3001/watr-fluid_itswatr.mp3",
+    url: "http://localhost:3001/watr-fluid_itswatr.mp3"
   },
 ];
 
@@ -50,7 +104,7 @@ export enum MOODS {
 }
 
 export enum BeatbarPlayerErrorNames {
-  INTERNAL_SERVER_ERROR= 'Internal Server Error',
+  INTERNAL_SERVER_ERROR = 'Internal Server Error',
   RESOURCE_NOT_FOUND = 'Resource not found',
   AUTHENTICATION_ERROR = 'Authentication Error',
   AUTHORIZATION_ERROR = 'Authorization Error',
