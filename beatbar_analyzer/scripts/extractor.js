00146a5e3718c267e8b7f7a4d97b67c8ee9be42b
toec9034 <tobias.eckert@study.hs-duesseldorf.de>
2023-09-11 14:02:45 +0200

    feat(beatbar_analyzer): update analyze algorithms and posting

--- beatbar_analyzer/scripts/extractor.js
+++ beatbar_analyzer/scripts/extractor.js
@@ -1,69 +1,140 @@
 // Globals
 
+const localCdnUrl = "http://localhost:3001/"
+
+const allFileNames = [
+  "aesthetics_sould-prod-music.mp3",
+  "bathroom-chill-background-music_chillmore.mp3",
+  "chillhop-beat_music-unlimited.mp3",
+  "close_sould-prod-music.mp3",
+  "coding-night_fassounds.mp3",
+  "coffee-chill-out_romanbelov.mp3",
+  "dont-stop-me-abstract-future-bass_alexi-action.mp3",
+  "embrace_itswatr.mp3",
+  "empty-mind_lofi-hour.mp3",
+  "goat_prazkhanal.mp3",
+  "good-night_fassounds.mp3",
+  "in-the-room_lofi-hour.mp3",
+  "jazz-cafe_fassounds.mp3",
+  "jazzy-hip-hop-boom-bap_music-unlimited.mp3",
+  "lofi-beat-chill_watrfallkero.mp3",
+  "lofi-chill-medium-version_bo-dleasons.mp3",
+  "lofi-girl-dreams_chillmore.mp3",
+  "lofi-piano-beat_tuesday-night.mp3",
+  "lofi-study_fassounds.mp3",
+  "lost-ambient-lofi-60s_lesfm.mp3",
+  "rain-and-nostalgia-version-60s_lesfm.mp3",
+  "sleepy-cat_lofi-hour.mp3",
+  "spirit-blossom_roman-belov.mp3",
+  "storm-clouds_purpple-cat.mp3",
+  "street-food_fassounds.mp3",
+  "sunset-vibes-lo-fichillhop_coma-media.mp3",
+  "sweet-chillhop_music-unlimited.mp3",
+  "sweet-love_day-fox.mp3",
+  "the-weekend_chillmore.mp3",
+  "tvari-tokyo-cafe_tvari.mp3",
+  "watr-fluid_itswatr.mp3",
+]
+
 let fetchUrl = ""
 let allowAudioContext = false;
+const essentia_properties = {
+  "key": "x",
+  "scale": "x",
+  "key_scale_strength": 1,
+  "bpm": 100,
+  "energy": 200,
+  "danceability": 300
+}
+
+const httpRequest = new XMLHttpRequest();
+
+function handler(event) {
+  console.log(event.target.response)
+}
+
+httpRequest.onreadystatechange = handler;
+
+let postUrl = ""
+let apiToken = "YEGmtC4QKGIQQK4qGuRD55q48JIkKazWEnDvQIcDYaA="
+
+const postRouteInput = document.getElementById('post-route')
+if(postRouteInput){
+  postRouteInput.addEventListener('input', (e)=>{
+    setPostUrl(e)
+  })
+}
+
+const apiTokenTextarea = document.getElementById('post-header')
+if(apiTokenTextarea){
+  apiTokenTextarea.addEventListener('input', (e)=>{
+    setApiToken(e)
+  })
+}
 
 const progessSpinner = document.getElementById('progress-spinner')
 
 const continueButton = document.getElementById('continue-button')
-if(continueButton){
+if (continueButton) {
   continueButton.disabled = true;
-  continueButton.addEventListener('click', ()=> continueHandler())
+  continueButton.addEventListener('click', () => continueHandler())
 }
 
 const displayInputUrl = document.getElementById('output-input-url')
-if(!displayInputUrl){
+if (!displayInputUrl) {
   console.error("the element with the id 'output-input-url' could not be found in the dom")
 }
 
 const displayKey = document.getElementById('output-key')
-if(!displayKey){
+if (!displayKey) {
   console.error("the element with the id 'output-key' could not be found in the dom")
 }
 
 const displayScale = document.getElementById('output-scale')
-if(!displayScale){
+if (!displayScale) {
   console.error("the element with the id 'output-scale' could not be found in the dom")
 }
 
 const displayKeyScaleStrength = document.getElementById('output-key-scale-strength')
-if(!displayKeyScaleStrength){
+if (!displayKeyScaleStrength) {
   console.error("the element with the id 'output-key-scale-strength' could not be found in the dom")
 }
 
 const displayDuration = document.getElementById('output-duration')
-if(!displayDuration){
+if (!displayDuration) {
   console.error("the element with the id 'output-duration' could not be found in the dom")
 }
 
 const displayBpm = document.getElementById('output-bpm')
-if(!displayBpm){
+if (!displayBpm) {
   console.error("the element with the id 'output-bpm' could not be found in the dom")
 }
 
 const displayEnergy = document.getElementById('output-energy')
-if(!displayEnergy){
+if (!displayEnergy) {
   console.error("the element with the id 'output-energy' could not be found in the dom")
 }
 
 const displayDanceability = document.getElementById('output-danceability')
-if(!displayDanceability){
+if (!displayDanceability) {
   console.error("the element with the id 'output-danceability' could not be found in the dom")
 }
 
 const setInputButton = document.getElementById('set-input-button')
-if(setInputButton){
+if (setInputButton) {
   const inputUrl = document.getElementById('input-url')
-  if(inputUrl){
-    setInputButton.addEventListener('click', ()=> {
+  if (inputUrl) {
+    setInputButton.addEventListener('click', () => {
       fetchUrl = inputUrl.value.toString()
+      postRouteInput.value = "http://localhost:8000/api/post/properties/?song_id=" + fetchUrl.split('/')[3]
+      postUrl = "http://localhost:8000/api/post/properties/?song_id=" + fetchUrl.split('/')[3]
       displayInputUrl.innerHTML = inputUrl.value.toString()
       displayInputUrl.href = inputUrl.value.toString()
     })
   }
 }
 
-function handleAllowAudioContext(event){
+function handleAllowAudioContext(event) {
   allowAudioContext = event.target.checked
   continueButton.disabled = !allowAudioContext;
 
@@ -72,19 +143,19 @@ function handleAllowAudioContext(event){
 const audioCtx = new AudioContext();
 let essentia;
 
-EssentiaWASM().then( (EssentiaWasm)=>{
+EssentiaWASM().then((EssentiaWasm) => {
   essentia = new Essentia(EssentiaWasm);
 
   // prints version of the essentia wasm backend
   console.log('essentia.js successfully loaded in Version: ', essentia.version)
 })
 
-function continueHandler(){
+function continueHandler() {
   console.log("Starting")
-  if(progessSpinner){
+  if (progessSpinner) {
     progessSpinner.classList.remove('hidden')
   }
-  audioCtx.resume().then(async (res)=>{
+  audioCtx.resume().then(async (res) => {
     console.log("Audio Context has been resumed! ", res)
     await analyze()
   })
@@ -116,16 +187,66 @@ async function analyze() {
   const computedEnergy = await essentia.Energy(inputSignalVector);
 
   displayKey.innerHTML = JSON.stringify(computedKeys.key)
+  essentia_properties.key = computedKeys.key
   displayScale.innerHTML = JSON.stringify(computedKeys.scale)
+  essentia_properties.scale = computedKeys.scale
   displayKeyScaleStrength.innerHTML = JSON.stringify(computedKeys.strength)
+  essentia_properties.key_scale_strength = computedKeys.strength;
 
   displayDuration.innerHTML = JSON.stringify(computedDuration.duration)
+  //duration is stored somewhere else
   displayBpm.innerHTML = JSON.stringify(computedTempo.bpm)
+  essentia_properties.bpm = computedTempo.bpm
 
   displayEnergy.innerHTML = JSON.stringify(computedEnergy.energy)
+  essentia_properties.energy = computedEnergy.energy
   displayDanceability.innerHTML = JSON.stringify(computedDanceability.danceability)
+  essentia_properties.danceability = computedDanceability.danceability
 
-  if(progessSpinner){
+  if (progessSpinner) {
     progessSpinner.classList.add('hidden')
   }
+}
+
+function setPostUrl(e){
+  console.log("setting post route: ", e.target.value)
+  postUrl = e.target.value
+}
+
+function setApiToken(e){
+  console.log("setting api token: ", e.target.value)
+  apiToken = e.target.value
+}
+
+function postResults() {
+  console.log("doing request")
+  console.log("url", postUrl)
+  console.log("x-beatbar-apitoken", apiToken)
+  console.log("body", JSON.stringify({
+    essentia_properties: {
+      "key": essentia_properties.key,
+      "scale": essentia_properties.scale,
+      "key_scale_strength": essentia_properties.key_scale_strength,
+      "bpm": essentia_properties.bpm,
+      "energy": essentia_properties.energy,
+      "danceability": essentia_properties.danceability
+    }
+  }))
+
+  if(fetchUrl.split('/')[3]){
+    httpRequest.open("POST", postUrl, true);
+    httpRequest.setRequestHeader('X-Beatbar-ApiToken', apiToken)
+    httpRequest.setRequestHeader('Content-Type', 'application/json')
+    httpRequest.send(JSON.stringify({
+      essentia_properties: {
+        "key": essentia_properties.key,
+        "scale": essentia_properties.scale,
+        "key_scale_strength": essentia_properties.key_scale_strength,
+        "bpm": essentia_properties.bpm,
+        "danceability": essentia_properties.danceability,
+        "energy": essentia_properties.energy,
+      }
+    }));
+  }
+
 }
